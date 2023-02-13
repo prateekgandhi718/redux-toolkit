eacd4d68f84cafe5f4165d0e516671f6ab9a698e
Prateek Gandhi <Prateek.Gandhi@lntinfotech.com>
2023-02-13 16:43:03 +0530

    middleware

--- app/store.js
+++ app/store.js
@@ -1,12 +1,18 @@
 const configureStore = require('@reduxjs/toolkit').configureStore
+
+const reduxLogger = require('redux-logger')
+
 const cakeReducer = require('../features/cake/cakeSlice')
 const iceCreamReducer = require('../features/icecream/iceCreamSlice')
+
+const logger = reduxLogger.createLogger()
 
 const store = configureStore({
     reducer : {
         cake: cakeReducer,
         icecream: iceCreamReducer
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 })
 
 module.exports = store
--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const cakeActions = require('./features/cake/cakeSlice').cakeActions
 const iceCreamActions = require('./features/icecream/iceCreamSlice').iceCreamActions
 
 console.log('Initial State: ', store.getState())
-const unsubscribe = store.subscribe(() => {
-    console.log('Updated State: ', store.getState())
-})
+const unsubscribe = store.subscribe(() => {})
 
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
